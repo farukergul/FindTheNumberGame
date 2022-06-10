4ee9d61cd16a7a146a4f79eed8a380c342b2dcae
farukergul <102088708+farukergul@users.noreply.github.com>
2022-06-10 21:19:57 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+const number = Math.trunc(Math.random() * 100);
+let sayac = 0;
+input.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    btn.click();
+    input.value = "";
+  }
+});
+faruk();
+function faruk() {
+  btn.addEventListener("click", () => {
+    let input = document.getElementById("input");
+    console.log(number);
+    while (sayac < 5) {
+      console.log(input.value);
+      if (Number(input.value) == number) {
+        sayac = 5;
+        return (output.innerHTML = "Tebrikler,Bildiniz.");
+      } else if (input.value < number) {
+        sayac = sayac + 1;
+        if (sayac == 5) {
+          return (output.innerHTML = "Kaybettiniz.");
+        } else {
+          return (output.innerHTML = "Daha büyük bir sayı seçiniz.");
+        }
+      } else {
+        sayac = sayac + 1;
+        if (sayac == 5) {
+          return (output.innerHTML = "Kaybettiniz.");
+        } else {
+          return (output.innerHTML = "Daha küçük bir sayı seçiniz.");
+        }
+      }
+    }
+  });
+}
